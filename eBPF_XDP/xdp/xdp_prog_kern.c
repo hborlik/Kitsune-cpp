485c195a0bfcc85c0d05fc1cfb04725da28bec2d
Hunter Borlik <6496875+hborlik@users.noreply.github.com>
2023-03-03 10:24:33 -0800

    packet parsing again

--- eBPF_XDP/xdp/xdp_prog_kern.c
+++ eBPF_XDP/xdp/xdp_prog_kern.c
@@ -22,63 +22,63 @@
 SEC("xdp")
 int  xdp_stats_func(struct xdp_md *ctx)
 {
-	// void *data_end = (void *)(long)ctx->data_end;
-	// void *data     = (void *)(long)ctx->data;
+	void *data_end = (void *)(long)ctx->data_end;
+	void *data     = (void *)(long)ctx->data;
 	int action = XDP_PASS;
 
-	// /**
-	//  * Parsing structures 
-	//  */
+	/**
+	 * Parsing structures 
+	 */
 
-	// struct ethhdr *eth;
-	// struct iphdr *iphdr;
-	// struct ipv6hdr *ipv6hdr;
-	// struct udphdr *udphdr;
-	// struct tcphdr *tcphdr;
-	// int eth_type, ip_type;
-	// struct hdr_cursor nh = {.pos = data};
+	struct ethhdr *eth;
+	struct iphdr *iphdr;
+	struct ipv6hdr *ipv6hdr;
+	struct udphdr *udphdr;
+	struct tcphdr *tcphdr;
+	int eth_type, ip_type;
+	struct hdr_cursor nh = {.pos = data};
 	__u64 timestamp = bpf_ktime_get_ns() / 1000; // microseconds
 
-	// /* Packet parsing in steps: Get each header one at a time, aborting if
-	//  * parsing fails. Each helper function does sanity checking (is the
-	//  * header type in the packet correct?), and bounds checking.
-	//  */
-	// eth_type = parse_ethhdr(&nh, data_end, &eth);
-	// if (eth_type < 0) {
-	// 	action = XDP_ABORTED;
-	// 	goto out;
-	// }
+	/* Packet parsing in steps: Get each header one at a time, aborting if
+	 * parsing fails. Each helper function does sanity checking (is the
+	 * header type in the packet correct?), and bounds checking.
+	 */
+	eth_type = parse_ethhdr(&nh, data_end, &eth);
+	if (eth_type < 0) {
+		action = XDP_ABORTED;
+		goto out;
+	}
 
 
-	// if (eth_type == bpf_htons(ETH_P_IP)) {
-	// 	ip_type = parse_iphdr(&nh, data_end, &iphdr);
-	// } else if (eth_type == bpf_htons(ETH_P_IPV6)) {
-	// 	ip_type = parse_ip6hdr(&nh, data_end, &ipv6hdr);
-	// } else {
-	// 	goto out;
-	// }
+	if (eth_type == bpf_htons(ETH_P_IP)) {
+		ip_type = parse_iphdr(&nh, data_end, &iphdr);
+	} else if (eth_type == bpf_htons(ETH_P_IPV6)) {
+		ip_type = parse_ip6hdr(&nh, data_end, &ipv6hdr);
+	} else {
+		goto out;
+	}
 
 
-	// if (ip_type == IPPROTO_UDP) {
-	// 	if (parse_udphdr(&nh, data_end, &udphdr) < 0) {
-	// 		action = XDP_ABORTED;
-	// 		goto out;
-	// 	}
-	// 	// rewrite destination port
-	// 	// udphdr->dest = bpf_htons(bpf_ntohs(udphdr->dest) - 1);
-	// } else if (ip_type == IPPROTO_TCP) {
-	// 	if (parse_tcphdr(&nh, data_end, &tcphdr) < 0) {
-	// 		action = XDP_ABORTED;
-	// 		goto out;
-	// 	}
-	// 	// rewrite destination port
-	// 	// tcphdr->dest = bpf_htons(bpf_ntohs(tcphdr->dest) - 1);
-	// } else if (ip_type == IPPROTO_ICMPV6) {
+	if (ip_type == IPPROTO_UDP) {
+		if (parse_udphdr(&nh, data_end, &udphdr) < 0) {
+			action = XDP_ABORTED;
+			goto out;
+		}
+		// rewrite destination port
+		// udphdr->dest = bpf_htons(bpf_ntohs(udphdr->dest) - 1);
+	} else if (ip_type == IPPROTO_TCP) {
+		if (parse_tcphdr(&nh, data_end, &tcphdr) < 0) {
+			action = XDP_ABORTED;
+			goto out;
+		}
+		// rewrite destination port
+		// tcphdr->dest = bpf_htons(bpf_ntohs(tcphdr->dest) - 1);
+	} else if (ip_type == IPPROTO_ICMPV6) {
 
-	// } else if (ip_type == IPPROTO_ICMP) {
+	} else if (ip_type == IPPROTO_ICMP) {
 		
-	// }
-// out:
+	}
+out:
 
 	if (inc_stat_insert(21, 15 << 16, timestamp) < 0)
 		action = XDP_ABORTED;
